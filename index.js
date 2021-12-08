937e9ff527e4ba5c1acb7173e1640039186bf434
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-12-08 09:03:40 -0300

    resolve o 4

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const getTalkerIdMiddleware = require('./getTalkerIdMiddleware');
 const postLoginMiddleware = require('./postLoginMiddleware');
 const validatePassword = require('./validatePassword');
 const validateEmail = require('./validateEmail');
+const postTalkerMiddleware = require('./postTalkerMiddleware');
+const validateToken = require('./validateToken');
+const validateName = require('./validateName');
+const validateAge = require('./validateAge');
+const validateWatchedAt = require('./validateWatchedAt');
+const validateRate = require('./validateRate');
+const validateTalk = require('./validateTalk');
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,6 +27,14 @@ app.get('/', (_request, response) => {
 app.get('/talker', getTalkerMiddleware);
 app.get('/talker/:id', getTalkerIdMiddleware);
 app.post('/login', validateEmail, validatePassword, postLoginMiddleware);
+app.post('/talker',
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
+  postTalkerMiddleware);
 
 app.listen(PORT, () => {
   console.log('Online');
