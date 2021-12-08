e731d755d49eee0a0ab51d5a4c72031dbf99562f
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-12-08 10:19:05 -0300

    faz o 6 (agora sim funciona)

--- getSearchMiddleware.js
+++ getSearchMiddleware.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 
 module.exports = (req, res, next) => {
-  const data = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
-  const { q } = req.query;
-  console.log(q);
+  let data = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+  let { q } = req.query;
+  if (!q) { q = ''; }
+  data = data.filter((person) => person.name.includes(q));
   if (data) { return res.status(200).json(data); }
   next();
 };
