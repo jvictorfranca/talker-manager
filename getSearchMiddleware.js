95e15d74223feea63fc21a3fcfc7237a74c72c56
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-12-08 10:25:04 -0300

    adiciona case insensitive so pra ficar mais legal

--- getSearchMiddleware.js
+++ getSearchMiddleware.js
@@ -4,7 +4,8 @@ module.exports = (req, res, next) => {
   let data = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
   let { q } = req.query;
   if (!q) { q = ''; }
-  data = data.filter((person) => person.name.includes(q));
+  q = q.toLowerCase();
+  data = data.filter((person) => person.name.toLowerCase().includes(q));
   if (data) { return res.status(200).json(data); }
   next();
 };
